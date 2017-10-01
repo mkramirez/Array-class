4e024d5729f64156a1ad0b643302eea6d7bc5896
mkramirez1992@gmail.com <mkramirez1992@gmail.com>
2017-10-01 15:20:23 -0700

    Test Commit

--- mainTest.cpp
+++ mainTest.cpp
@@ -6,45 +6,69 @@ using namespace std;
 
 template <class T>
 class Array {
-    T values [3]; //Number can be fixed to whatever you want.
-
 public:
-    Array(T first, string second, double third){
-        values[0] = first;
-        values[1] = second;
-        values[2] = third;
-    }
-};
+    Array(T size);
+    ~Array();
+    int & operator[](T i) const;
+    int size() const;
+    void fill(T k);
+    int * begin();
+    int * end();
+    //int push(const T&);
+    //int pop(T&);
 
-class testSpam {
-public:
-    testSpam(int * helper1, string * helper2, double * helper3);
-    ~testSpam();
-    //void fill(int j, string k, double l);
-    //int helper1();
-    //string helper2();
-    //double helper3();
-    /*int 아 = 1;
-    string 애 = "안녕";
-    double 야 = 0.2; */
 private:
-    int * spam1;
-    string * spam2;
-    double * spam3;
+    T len;
+    T * data;
 };
 
-testSpam::testSpam(int *helper1, string *helper2, double *helper3) {
-    spam1 = helper1;
-    spam2 = helper2;
-    spam3 = helper3;
+template <class T>
+Array<T>::Array(T size) {
+    len = size;
+    data = new int[len];
 }
 
-testSpam::~testSpam() {
-    delete [] spam1, spam2, spam3;
+template <class T>
+Array<T>::~Array(){
+    delete [] data;
 }
 
+template <class T>
+int &Array<T>::operator[](T i) const {
+    return data [i];
+}
+
+template <class T>
+int Array<T>::size() const {
+    return len;
+}
+
+template <class T>
+void Array<T>::fill(T k) {
+    for(T i = 0; i < len; i++){
+        data[i] = k;
+    }
+}
+
+template <class T>
+int *Array<T>::begin() {
+    return & data[0];
+}
+
+template <class T>
+int *Array<T>::end() {
+    return & data[len];
+}
 
 int main(int argc, char * args[]) {
+    Array <int> a(3);
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    assert(a[0] == 1);
+    assert(a[1] == 2);
+    assert(a[2] == 3);
 
-
+    cout << "All tests passed." << endl;
+    return 0;
 }
