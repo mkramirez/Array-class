9bd0bae2738189b50516309c46117f210bc104b4
mkramirez1992@gmail.com <mkramirez1992@gmail.com>
2017-10-01 20:46:46 -0700

    4th Commit, changed more variables to T

--- mainTest.cpp
+++ mainTest.cpp
@@ -9,11 +9,11 @@ class Array {
 public:
     Array(T size);
     ~Array();
-    int & operator[](T i) const;
-    int size() const;
+    T & operator[](T i) const;
+    T size() const;
     void fill(T k);
-    int * begin();
-    int * end();
+    T * begin();
+    T * end();
     //int push(const T&);
     //int pop(T&);
 
@@ -25,7 +25,7 @@ private:
 template <class T>
 Array<T>::Array(T size) {
     len = size;
-    data = new int[len];
+    data = new T[len];
 }
 
 template <class T>
@@ -34,12 +34,12 @@ Array<T>::~Array(){
 }
 
 template <class T>
-int &Array<T>::operator[](T i) const {
+T &Array<T>::operator[](T i) const {
     return data [i];
 }
 
 template <class T>
-int Array<T>::size() const {
+T Array<T>::size() const {
     return len;
 }
 
@@ -51,24 +51,42 @@ void Array<T>::fill(T k) {
 }
 
 template <class T>
-int *Array<T>::begin() {
+T *Array<T>::begin() {
     return & data[0];
 }
 
 template <class T>
-int *Array<T>::end() {
+T *Array<T>::end() {
     return & data[len];
 }
 
 int main(int argc, char * args[]) {
     Array <int> a(3);
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
+    a[0] = 10;
+    a[1] = 11;
+    a[2] = 12;
+    assert(a[0] == 10);
+    assert(a[1] == 11);
+    assert(a[2] == 12);
+    assert(a.size() == 3);
+    a.fill(5);
+    assert(a[0] == 5);
+    assert(a[1] == 5);
+    assert(a[2] == 5);
+    a[0] = 3;
+    a[1] = 1;
+    a[2] = 2;
+    sort(a.begin(), a.end());
     assert(a[0] == 1);
     assert(a[1] == 2);
     assert(a[2] == 3);
-
+    int * it = a.begin();
+    assert(*it == 1);
+    ++it;
+    assert(*it == 2);
+    ++it;
+    assert(*it == 3);
+    ++it;
+    assert(it == a.end());
     cout << "All tests passed." << endl;
-    return 0;
 }
